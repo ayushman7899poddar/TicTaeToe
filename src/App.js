57c07bf64ae045e22b06cc0cfe0f3f8098ac0abe
ayushman7899poddar <113877684+ayushman7899poddar@users.noreply.github.com>
2024-02-04 11:03:53 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Board from './TicTacToeGame/Board';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>TIC TAC TOE GAME</h1>
+    
+      <Board/>
+    </div>
+  );
+}
+
+export default App;
